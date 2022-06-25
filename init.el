91db1c4470092a9dd15936997ca73a487b6cbeba
annnenpolka <k.miyakewiss1@gmail.com>
2022-06-25 19:58:06 +0900

    add evil-mc hydra config

--- init.el
+++ init.el
@@ -334,6 +334,7 @@
     ("C-l" . 'evil-ex-nohighlight)
     ("zg" . flyspell-correct-at-point)
     ("<leader>SPC" . 'consult-buffer)
+    ("<leader>n" . 'my-mc-hydra/body)
     ("<leader>h" . windmove-left)
     ("<leader>j" . windmove-down)
     ("<leader>k" . windmove-up)
@@ -374,18 +375,120 @@
   :ensure t
   :global-minor-mode global-evil-mc-mode
   :config
+    (defun evil-mc-toggle-cursors ()
+    "Toggle frozen state of evil-mc cursors."
+    (interactive)
+    (unless (evil-mc-has-cursors-p)
+        (user-error "No cursors exist to be toggled"))
+    (setq evil-mc-frozen (not (and (evil-mc-has-cursors-p)
+                                    evil-mc-frozen)))
+    (if evil-mc-frozen
+        (message "evil-mc paused")
+        (message "evil-mc resumed")))
+
+    (evil-define-command evil-mc-toggle-cursor-here ()
+    "Create a cursor at point. If in visual block or line mode, then create
+    cursors on each line of the selection, on the column of the cursor. Otherwise
+    pauses cursors."
+    :repeat nil
+    :keep-visual nil
+    :evil-mc t
+    (interactive)
+    (cond ((and (evil-mc-has-cursors-p)
+                (evil-normal-state-p)
+                (let* ((pos (point))
+                        (cursor (cl-find-if (lambda (cursor)
+                                            (eq pos (evil-mc-get-cursor-start cursor)))
+                                            evil-mc-cursor-list)))
+                    (when cursor
+                    (evil-mc-delete-cursor cursor)
+                    (setq evil-mc-cursor-list (delq cursor evil-mc-cursor-list))
+                    t))))
+
+            ((memq evil-this-type '(block line))
+            (let ((col (evil-column))
+                (line-at-pt (line-number-at-pos)))
+            ;; Fix off-by-one error
+            (when (= evil-visual-direction 1)
+                (cl-decf col)
+                (backward-char))
+            (save-excursion
+                (evil-apply-on-block
+                (lambda (ibeg _)
+                    (unless (or (= line-at-pt (line-number-at-pos ibeg))
+                                (invisible-p ibeg))
+                    (goto-char ibeg)
+                    (move-to-column col)
+                    (when (= (current-column) col)
+                        (evil-mc-make-cursor-here))))
+                evil-visual-beginning
+                (if (eq evil-this-type 'line) (1- evil-visual-end) evil-visual-end)
+                nil)
+                (evil-exit-visual-state))))
+            (t
+            (evil-mc-pause-cursors)
+            ;; I assume I don't want the cursors to move yet
+            (evil-mc-make-cursor-here))))
+
+    (evil-define-command evil-mc-undo-cursor ()
+    "Undos last cursor, or all cursors in visual region."
+    :repeat nil
+    :evil-mc t
+    (interactive)
+    (if (evil-visual-state-p)
+        (or (mapc (lambda (c)
+                    (evil-mc-delete-cursor c)
+                    (setq evil-mc-cursor-list (delq c evil-mc-cursor-list)))
+                    (cl-remove-if-not
+                    (lambda (pos)
+                    (and (>= pos evil-visual-beginning)
+                            (<  pos evil-visual-end)))
+                    evil-mc-cursor-list
+                    :key #'evil-mc-get-cursor-start))
+            (message "No cursors to undo in region"))
+        (evil-mc-undo-last-added-cursor)))
+
   ;; smartparens integration
    (dolist (sp-command
             '(sp-up-sexp sp-copy-sexp sp-down-sexp sp-join-sexp sp-kill-sexp sp-next-sexp sp-split-sexp sp-wrap-curly sp-wrap-round sp-raise-sexp sp-clone-sexp sp-wrap-square sp-splice-sexp sp-end-of-sexp sp-forward-sexp sp-backward-sexp sp-convolute-sexp sp-transpose-sexp sp-kill-whole-line sp-beginning-of-sexp sp-forward-barf-sexp sp-forward-slurp-sexp sp-backward-barf-sexp sp-backward-slurp-sexp sp-splice-sexp-killing-forward sp-splice-sexp-killing-backward))
      (add-to-list 'evil-mc-custom-known-commands
                   `(,sp-command
-                    (:default . evil-mc-execute-call)))))
+                    (:default . evil-mc-execute-call))))
+   :hydra
+    (my-mc-hydra (:color pink
+                        :hint nil
+                        :pre (evil-mc-pause-cursors))
+    "
+    ^Match^            ^Line-wise^           ^Manual^
+    ^^^^^^----------------------------------------------------
+    _Z_: match all     _J_: make & go down   _z_: toggle here
+    _m_: make & next   _K_: make & go up     _r_: remove last
+    _M_: make & prev   ^ ^                   _R_: remove all
+    _n_: skip & next   ^ ^                   _p_: pause/resume
+    _N_: skip & prev
+
+    Current pattern: %`evil-mc-pattern
+
+    "
+    ("Z" #'evil-mc-make-all-cursors)
+    ("m" #'evil-mc-make-and-goto-next-match)
+    ("M" #'evil-mc-make-and-goto-prev-match)
+    ("n" #'evil-mc-skip-and-goto-next-match)
+    ("N" #'evil-mc-skip-and-goto-prev-match)
+    ("J" #'evil-mc-make-cursor-move-next-line)
+    ("K" #'evil-mc-make-cursor-move-prev-line)
+    ("z" #'evil-mc-toggle-cursor-here)
+    ("r" #'evil-mc-undo-cursor)
+    ("R" #'evil-mc-undo-all-cursors)
+    ("p" #'evil-mc-toggle-cursors)
+    ("q" #'evil-mc-resume-cursors "quit" :color blue)
+    ("<escape>" #'evil-mc-resume-cursors "quit" :color blue)))
 
 (leaf key-chord
   :ensure t
   :global-minor-mode t
   :custom
-  (key-chord-two-keys-delay . 0.1)
+  (key-chord-two-keys-delay . 0.15)
   ;(key-chord-one-key-delay . 0.1)
   :config
   (key-chord-define evil-insert-state-map "jk" 'evil-normal-state))
