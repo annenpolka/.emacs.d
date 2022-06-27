d55a79fbdb189e04a29e185c412a28f9b83ef727
annnenpolka <k.miyakewiss1@gmail.com>
2022-06-27 13:09:47 +0900

    set evil-escape-excluded-states

--- init.el
+++ init.el
@@ -662,6 +662,7 @@
   :setq-default
   (evil-escape-key-sequence . "jk")
   (evil-escape-delay . 0.2)
+  (evil-escape-excluded-states . '(normal visual motion emacs))
   (evil-escape-excluded-major-modes
     .
     '
