76f06450693280b1708c7089ba92c68ac1ef82b5
Annenpolka <lancelbb@gmail.com>
2024-05-07 14:29:13 +0900

    initial commit

---
+++ early-init.el
@@ -0,0 +1,6 @@
+;;; early-init.el - Annenpolka's emacs early-init.el
+;;; Commentary:
+;;; Code: Annenpolka
+
+(provide 'early-init)
+;;; early-init.el ends here
---
+++ init.el
@@ -0,0 +1,7 @@
+;;; init.el --- Annenpolka's emacs init.el
+;;; Commentary:
+;;; Code: Annenpolka
+
+
+(provide 'init)
+;;; init.el ends here
