5645536c39d1302d2d8e1fa237c8b4446132fb56
annenpolka <lancelbb@gmail.com>
2024-05-12 21:05:16 +0900

    add prot-window and related modules

--- init.el
+++ init.el
@@ -27,6 +27,21 @@
       package-native-compile t           ; インストール時にnative compileする
       )
 
+;; ローカルのパッケージパスを通す ----------------------------------------------------------
+(mapc
+ (lambda (string)
+   (add-to-list 'load-path (locate-user-emacs-file string)))
+ '("modules"))
+
+(use-package prot-common
+  :ensure nil
+  :functions (prot-common-truncate-lines-silently)
+  :hook ((fundamental-mode text-mode prog-mode) . prot-common-truncate-lines-silently)
+  :config
+  ;; NEVER tell me which key can call a command that I specifically
+  ;; invoked with M-x: I have a good reason to use it that way.
+  (advice-add #'execute-extended-command--describe-binding-msg :override #'prot-common-ignore))
+
 ;; use-package and related ones ---------------------------------------------------------
 (use-package use-package
   :config
@@ -44,8 +59,7 @@
   (auto-compile-on-load-mode +1)
   (auto-compile-on-save-mode +1))
 
-
-;; enacs built-in configs ---------------------------------------------------------------
+;; emacs built-in configs ---------------------------------------------------------------
 (use-package emacs
   :ensure nil
   :bind* (("M-ESC ESC" . c/redraw-frame)
@@ -136,7 +150,86 @@
 (unless IS-LINUX
   (setq command-line-x-option-alist nil))
 
+;; `window', `display-buffer-alist', and related ----------------------------------------
+(use-package prot-window
+  :ensure nil
+  :demand t
+  :config
+  (setq display-buffer-alist
+        `(;; no window
+          ("\\`\\*Async Shell Command\\*\\'"
+           (display-buffer-no-window))
+          ("\\`\\*\\(Warnings\\|Compile-Log\\|Org Links\\)\\*\\'"
+           (display-buffer-no-window)
+           (allow-no-window . t))
+          ;; bottom side window
+          ("\\*Org \\(Select\\|Note\\)\\*" ; the `org-capture' key selection and `org-add-log-note'
+           (display-buffer-in-side-window)
+           (dedicated . t)
+           (side . bottom)
+           (slot . 0)
+           (window-parameters . ((mode-line-format . none))))
+          ;; bottom buffer (NOT side window)
+          ((or . ((derived-mode . flymake-diagnostics-buffer-mode)
+                  (derived-mode . flymake-project-diagnostics-mode)
+                  (derived-mode . messages-buffer-mode)
+                  (derived-mode . backtrace-mode)))
+           (display-buffer-reuse-mode-window display-buffer-at-bottom)
+           (window-height . 0.3)
+           (dedicated . t)
+           (preserve-size . (t . t)))
+          ("\\*Embark Actions\\*"
+           (display-buffer-reuse-mode-window display-buffer-below-selected)
+           (window-height . fit-window-to-buffer)
+           (window-parameters . ((no-other-window . t)
+                                 (mode-line-format . none))))
+          ("\\*\\(Output\\|Register Preview\\).*"
+           (display-buffer-reuse-mode-window display-buffer-at-bottom))
+          ;; below current window
+          ("\\(\\*Capture\\*\\|CAPTURE-.*\\)"
+           (display-buffer-reuse-mode-window display-buffer-below-selected))
+          ("\\*\\vc-\\(incoming\\|outgoing\\|git : \\).*"
+           (display-buffer-reuse-mode-window display-buffer-below-selected)
+           (window-height . 0.1)
+           (dedicated . t)
+           (preserve-size . (t . t)))
+          ((derived-mode . reb-mode) ; M-x re-builder
+           (display-buffer-reuse-mode-window display-buffer-below-selected)
+           (window-height . 4) ; note this is literal lines, not relative
+           (dedicated . t)
+           (preserve-size . (t . t)))
+          ((or . ((derived-mode . occur-mode)
+                  (derived-mode . grep-mode)
+                  (derived-mode . Buffer-menu-mode)
+                  (derived-mode . log-view-mode)
+                  (derived-mode . help-mode) ; See the hooks for `visual-line-mode'
+                  "\\*\\(|Buffer List\\|Occur\\|vc-change-log\\|eldoc.*\\).*"
+                  prot-window-shell-or-term-p
+                  ;; ,world-clock-buffer-name
+                  ))
+           (prot-window-display-buffer-below-or-pop)
+           (body-function . prot-window-select-fit-size))
+          ("\\*\\(Calendar\\|Bookmark Annotation\\|ert\\).*"
+           (display-buffer-reuse-mode-window display-buffer-below-selected)
+           (dedicated . t)
+           (window-height . fit-window-to-buffer))
+          ;; NOTE 2022-09-10: The following is for `ispell-word', though
+          ;; it only works because I override `ispell-display-buffer'
+          ;; with `prot-spell-ispell-display-buffer' and change the
+          ;; value of `ispell-choices-buffer'.
+          ("\\*ispell-top-choices\\*.*"
+           (display-buffer-reuse-mode-window display-buffer-below-selected)
+           (window-height . fit-window-to-buffer))
+          ;; same window
 
+          ;; NOTE 2023-02-17: `man' does not fully obey the
+          ;; `display-buffer-alist'.  It works for new frames and for
+          ;; `display-buffer-below-selected', but otherwise is
+          ;; unpredictable.  See `Man-notify-method'.
+          ((or . ((derived-mode . Man-mode)
+                  (derived-mode . woman-mode)
+                  "\\*\\(Man\\|woman\\).*"))
+           (display-buffer-same-window)))))
 ;; =======================================================================================
 ;; IME
 ;; =======================================================================================
