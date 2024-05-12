2f0ea7d98b5124a7b18d5c83ac11bece6a81911f
annenpolka <lancelbb@gmail.com>
2024-05-12 22:00:34 +0900

    add corfu and meow dependency packages

--- init.el
+++ init.el
@@ -544,6 +544,105 @@
   ;; (server-after-make-frame-hook . set-japanese-font-face)
   :bind
   ("C-c F" . fontaine-set-preset))
+
+;; themes --------------------------------------------------------------------------------
+
+;; =======================================================================================
+;; suggestion/autocompletion
+;; =======================================================================================
+(use-package corfu
+  ;; Optional customizations
+  :custom
+  (corfu-cycle t)           ;; Enable cycling for `corfu-next/previous'
+  (corfu-preselect 'prompt) ;; Always preselect the prompt
+  (corfu-auto t)
+  (corfu-auto-delay 0.01)
+  (corfu-auto-prefix 3)
+  (corfu-cycle t)
+  (corfu-on-exact-match nil)
+  (tab-always-indent 'complete)
+  ;; Use TAB for cycling, default is `corfu-complete'.
+  :bind
+  (:map corfu-map
+	("TAB" . corfu-next)
+	([tab] . corfu-next)
+	("S-TAB" . corfu-previous)
+	([backtab] . corfu-previous))
+
+  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
+  ;; be used globally (M-/).  See also the customization variable
+  ;; `global-corfu-modes' to exclude certain modes.
+  :hook
+  (prog-mode . corfu-mode))
+
+;; Add extensions
+(use-package cape
+  ;; Bind dedicated completion commands
+  ;; Alternative prefix keys: C-c p, M-p, M-+, ...
+  :bind (("C-c p p" . completion-at-point) ;; capf
+         ("C-c p t" . complete-tag)        ;; etags
+         ("C-c p d" . cape-dabbrev)        ;; or dabbrev-completion
+         ("C-c p h" . cape-history)
+         ("C-c p f" . cape-file)
+         ("C-c p k" . cape-keyword)
+         ("C-c p s" . cape-elisp-symbol)
+         ("C-c p e" . cape-elisp-block)
+         ("C-c p a" . cape-abbrev)
+         ("C-c p l" . cape-line)
+         ("C-c p w" . cape-dict)
+         ("C-c p :" . cape-emoji)
+         ("C-c p \\" . cape-tex)
+         ("C-c p _" . cape-tex)
+         ("C-c p ^" . cape-tex)
+         ("C-c p &" . cape-sgml)
+         ("C-c p r" . cape-rfc1345))
+  :hook
+  (((prog-mode
+     text-mode) . my/set-super-capf))
+  :init
+  (defun my/set-super-capf (&optional arg)
+    (setq-local completion-at-point-functions
+                (list (cape-capf-noninterruptible
+		       (cape-capf-buster
+                        (cape-capf-properties
+                         (cape-capf-super
+                          (if arg
+			      arg
+                            (car completion-at-point-functions))
+                          ;; #'tempel-complete
+                          ;; #'tabnine-completion-at-point
+                          #'cape-dabbrev
+                          #'cape-file)
+                         :sort t
+                         :exclusive 'no))))))
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  (add-to-list 'completion-at-point-functions #'cape-file)
+  (add-to-list 'completion-at-point-functions #'cape-elisp-block)
+  ;;(add-to-list 'completion-at-point-functions #'cape-history)
+  (add-to-list 'completion-at-point-functions #'cape-keyword)
+  ;;(add-to-list 'completion-at-point-functions #'cape-tex)
+  ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
+  ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
+  ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
+  ;;(add-to-list 'completion-at-point-functions #'cape-dict)
+  ;;(add-to-list 'completion-at-point-functions #'cape-elisp-symbol)
+  ;;(add-to-list 'completion-at-point-functions #'cape-line)
+  )
+(use-package prescient
+  :config
+  (setq prescient-aggressive-file-save t)
+  (prescient-persist-mode +1))
+
+(use-package corfu-prescient
+  :after corfu
+  :config
+  (with-eval-after-load 'orderless
+    (setq corfu-prescient-enable-filtering nil))
+  (corfu-prescient-mode +1))
 ;; =======================================================================================
 ;; Editor
 ;; =======================================================================================
@@ -565,7 +664,11 @@
 ;; meow keymap ---------------------------------------------------------------------------
 (use-package meow
   :init
-  ;; command functions
+  ;; --- dependencies ---
+  (use-package expand-region)
+  (use-package embrace)
+
+  ;; --- command functions ---
   (defun meow-save-line nil
     (interactive)
     (meow-line 1)
@@ -592,7 +695,7 @@
 	(kill-this-buffer)
       (delete-window)))
 
-  ;; setup keymap
+  ;; --- setup/keymap ---
   (defun meow-setup nil
     (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
     (meow-motion-overwrite-define-key
