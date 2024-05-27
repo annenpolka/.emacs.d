eb7e2948d13906ee1302c2b3c521837fb9ebecc0
annenpolka <lancelbb@gmail.com>
2024-05-27 15:01:25 +0900

    doom-modeline: don’t use icons on Windows

--- init.el
+++ init.el
@@ -639,13 +639,19 @@
 ;; modeline -------------------------------------------------------------------------------
 (use-package doom-modeline
   :ensure t
-  :hook (after-init . doom-modeline-mode))
+  :hook (after-init . doom-modeline-mode)
+  :config
+  ;; emojis won’t work properly in Windows
+  (when IS-WINDOWS
+    (setq doom-modeline-icon nil)))
 (use-package nerd-icons
   ;; :custom
   ;; The Nerd Font you want to use in GUI
   ;; "Symbols Nerd Font Mono" is the default and is recommended
   ;; but you can use any other Nerd Font if you want
   ;; (nerd-icons-font-family "Symbols Nerd Font Mono")
+  :config
+  ;; (setq nerd-icons-color-icons nil)
   )
 ;; =======================================================================================
 ;; suggestion/autocompletion
