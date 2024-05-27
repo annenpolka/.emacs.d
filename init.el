ce6f5c828c49a7b64889daac9e751cc791f1bb38
annenpolka <lancelbb@gmail.com>
2024-05-27 11:54:25 +0900

    add org-modern

--- init.el
+++ init.el
@@ -1139,6 +1139,37 @@
   (setq org-priority-faces nil)
   )
 
+(use-package org-modern
+  :config
+  (setq
+   ;; Edit settings
+   org-auto-align-tags nil
+   org-tags-column 0
+   org-catch-invisible-edits 'show-and-error
+   org-special-ctrl-a/e t
+   org-insert-heading-respect-content t
+
+   ;; Org styling, hide markup etc.
+   org-hide-emphasis-markers t
+   org-pretty-entities t
+
+   ;; Agenda styling
+   org-agenda-tags-column 0
+   org-agenda-block-separator ?─
+   org-agenda-time-grid
+   '((daily today require-timed)
+     (800 1000 1200 1400 1600 1800 2000)
+     "       " "               ")
+   org-agenda-current-time-string
+   " ── now ─────────────────────────────────────────────────")
+
+  ;; Ellipsis styling
+  (setq org-ellipsis "…")
+  (set-face-attribute 'org-ellipsis nil :inherit 'default :box nil)
+  :hook
+  (org-mode . org-modern-mode)
+  (org-agenda-finalize . org-modern-agenda))
+
 ;; howm Hitori Otegaru Wiki Modoki -------------------------------------------------------
 (use-package howm
   :init
